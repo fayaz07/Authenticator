f818e3bc91c0435a2d62a52f1cfc68fc1d6139b1
Mohammad Fayaz <fayazfz07@gmail.com>
2021-11-07 16:44:54 +0530

    added socket service

--- app/src/main/java/in/mohammadfayaz/authenticator/services/MyWebSocketService.kt
+++ app/src/main/java/in/mohammadfayaz/authenticator/services/MyWebSocketService.kt
@@ -1,12 +1,54 @@
 package `in`.mohammadfayaz.authenticator.services
 
+import `in`.mohammadfayaz.authenticator.MyWebSocketClient
+import `in`.mohammadfayaz.authenticator.utils.EventData
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
+import androidx.lifecycle.LiveData
+import java.net.URI
 
 class MyWebSocketService : Service() {
 
-  override fun onBind(intent: Intent): IBinder {
-    TODO("Return the communication channel to the service.")
+  private val url = "ws://10.0.2.2:8080/"
+  private val socketClient: MyWebSocketClient = MyWebSocketClient(URI(url))
+
+  val TAG = "MyService"
+  val mBinder = MyBinder()
+
+  override fun onCreate() {
+    println("oncreate service")
+    super.onCreate()
   }
+
+  fun connectToSocket() {
+    //open websocket
+    socketClient.connect()
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    socketClient.close()
+  }
+
+  fun getEventListener(): LiveData<EventData> {
+    return socketClient.events
+  }
+
+  override fun onBind(intent: Intent): IBinder {
+    return mBinder
+  }
+
+  class MyBinder : Binder() {
+    private var instance: MyWebSocketService? = null
+    public fun getService() : MyWebSocketService{
+      if(instance == null)
+        instance = MyWebSocketService()
+      return instance!!
+    }
+  }
+
 }
